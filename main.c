349bb72da96914747b4b9abbc5f98a5ab07867a4
Omran Alhaddad <44875477+Omrna@users.noreply.github.com>
2020-12-12 21:17:02 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define SECRET1 0x44
+#define SECRET2 0x55
+
+int main(int argc, char *argv[])
+{
+    double x = 13.1222244444442;
+    double y = 13.1222244444443;
+    
+    if(x==y){
+      printf("X eqqual Y");
+    }else{
+      printf("X not eqqual Y");
+    }
+    return 0;
+}
